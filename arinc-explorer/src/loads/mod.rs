521e83fd0bea58a22c4b5e816dbdb0225914b88d
eagle941 <8973725+Eagle941@users.noreply.github.com>
2025-07-10 23:06:23 +0100

    feat: implemented LOADS.LUM parser

---
+++ arinc-explorer/src/loads/mod.rs
@@ -0,0 +1,155 @@
+use std::fmt::{self, Display, Formatter};
+use std::fs::File;
+use std::io::{Cursor, Read};
+use std::path::Path;
+
+use binrw::{binrw, BinRead};
+use lsp::Lsp;
+
+use crate::error::LoadsLumError;
+use crate::utils::{combine_words, vec16_to_string};
+
+mod lsp;
+mod target_hw;
+
+#[binrw]
+#[brw(big)]
+#[derive(Debug, Clone, Eq, PartialEq, Default)]
+pub struct LoadsLum {
+    file_length_msb: u16,
+    file_length_lsb: u16,
+    media_file_format_verion: u16,
+    //spare: u16, // Seems like the spare doesn't exist
+    pointer_to_media_set_pn_length_msb: u16,
+    pointer_to_media_set_pn_length_lsb: u16,
+    pointer_to_number_of_loads_msb: u16,
+    pointer_to_number_of_loads_lsb: u16,
+    pointer_to_user_defined_data_msb: u16,
+    pointer_to_user_defined_data_lsb: u16,
+    // Expansion point no1
+    media_set_pn_length: u16, // number of chars
+
+    #[br(count = media_set_pn_length.div_ceil(2))]
+    media_set_pn: Vec<u16>, // It is always an even length
+    media_sequence_number_x: u8,
+    number_of_media_set_members_y: u8,
+    number_of_loads: u16,
+
+    #[br(count = number_of_loads)]
+    loads: Vec<Lsp>,
+    // Expansion point no3
+    #[br(if(combine_words(pointer_to_user_defined_data_msb, pointer_to_user_defined_data_lsb)!=0), count = (combine_words(file_length_msb, file_length_lsb) - combine_words(pointer_to_user_defined_data_msb, pointer_to_user_defined_data_lsb) - 1).div_ceil(2))]
+    user_defined_data: Option<Vec<u16>>,
+
+    file_crc: u16,
+}
+impl LoadsLum {
+    /// Constructs a new [`LoadsLum`] object.
+    ///
+    /// The function doesn't have any protection against big file size and
+    /// attemps to read the whole file into the memory.
+    ///
+    /// # Arguments
+    ///
+    /// - `path`: the path to the `LOADS.LUM` file.
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Err`] if the file is missing or if there is a problem reading
+    /// the file into the [`LoadsLum`] struct.
+    pub fn new(path: &Path) -> Result<Self, LoadsLumError> {
+        let mut file = File::open(path)?;
+        let mut buf = Vec::new();
+        file.read_to_end(&mut buf)?;
+        let loads_lum = LoadsLum::read(&mut Cursor::new(buf))?;
+        Ok(loads_lum)
+    }
+
+    fn get_file_type_string(&self) -> String {
+        match self.media_file_format_verion {
+            0x8002..=0x8004 => "Load file".to_string(),
+            0x9004 => "Batch file".to_string(),
+            0xA004 => "Media file".to_string(),
+            x => format!("{x} Unrecognised file"),
+        }
+    }
+
+    // Returns the number of 16-bit words
+    #[must_use]
+    pub fn get_file_length(&self) -> u32 {
+        combine_words(self.file_length_msb, self.file_length_lsb)
+    }
+
+    // Returns the offset in 16-bit words from the beginning of the file
+    #[must_use]
+    pub fn get_pointer_to_media_set_pn_length(&self) -> u32 {
+        combine_words(
+            self.pointer_to_media_set_pn_length_msb,
+            self.pointer_to_media_set_pn_length_lsb,
+        )
+    }
+
+    // Returns the offset in 16-bit words from the beginning of the file
+    #[must_use]
+    pub fn get_pointer_to_number_of_loads(&self) -> u32 {
+        combine_words(
+            self.pointer_to_number_of_loads_msb,
+            self.pointer_to_number_of_loads_lsb,
+        )
+    }
+
+    // Returns the offset in 16-bit words from the beginning of the file
+    #[must_use]
+    pub fn get_pointer_to_user_defined_data(&self) -> u32 {
+        combine_words(
+            self.pointer_to_user_defined_data_msb,
+            self.pointer_to_user_defined_data_lsb,
+        )
+    }
+
+    #[must_use]
+    pub fn get_media_set_pn(&self) -> String {
+        // If media_set_pn_length is odd, an extra NUL byte is added at the end.
+        // This function removes the NUL byte if it exists.
+        vec16_to_string(&self.media_set_pn, self.media_set_pn_length as usize)
+    }
+}
+impl Display for LoadsLum {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{} Bytes\n{}\n{} | {} | {} Offsets\n{} Chars in Media Set PN\n{} Media Set PN\n{} \
+             Media number\n{} Total Media sets\n{} Total loads\n{}\n{:?} User Data\n0x{:x} CRC",
+            self.get_file_length() * 2,
+            self.get_file_type_string(),
+            self.get_pointer_to_number_of_loads(),
+            self.get_pointer_to_number_of_loads(),
+            self.get_pointer_to_user_defined_data(),
+            self.media_set_pn_length,
+            self.get_media_set_pn(),
+            self.media_sequence_number_x,
+            self.number_of_media_set_members_y,
+            self.number_of_loads,
+            self.loads[0],
+            self.user_defined_data,
+            self.file_crc
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use super::*;
+
+    #[ignore = "File not available"]
+    #[test]
+    fn test_simple_loads_lum() {
+        // 1 hex = 4 bits
+        // 4 hex = 16 bits = 2 bytes
+        let file = PathBuf::from("../../LOADS.LUM");
+        let loads_lum = LoadsLum::new(file.as_path()).unwrap();
+        println!("{loads_lum}");
+    }
+}
---
+++ arinc-explorer/src/loads/target_hw.rs
@@ -0,0 +1,32 @@
+use std::fmt::{self, Display, Formatter};
+
+use binrw::binrw;
+
+use crate::utils::vec16_to_string;
+
+#[binrw]
+#[brw(big)]
+#[derive(Debug, Clone, Eq, PartialEq, Default)]
+pub struct TargetHW {
+    target_hw_id_length: u16, // number of chars
+
+    #[br(count = target_hw_id_length.div_ceil(2))]
+    target_hw_id: Vec<u16>,
+}
+impl TargetHW {
+    pub fn get_target_hw_id(&self) -> String {
+        // If target_hw_id_length is odd, an extra NUL byte is added at the end.
+        // This function removes the NUL byte if it exists.
+        vec16_to_string(&self.target_hw_id, self.target_hw_id_length as usize)
+    }
+}
+impl Display for TargetHW {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(
+            f,
+            "\t\t{} Chars in Target HW ID\n\t\t{} Target HW ID",
+            self.target_hw_id_length,
+            self.get_target_hw_id(),
+        )
+    }
+}
