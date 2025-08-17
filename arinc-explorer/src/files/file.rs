2717db4898ed00adf7c63a09366d43d43bc3bf53
eagle941 <8973725+Eagle941@users.noreply.github.com>
2025-08-17 15:40:50 +0100

    feat: added parser for FILES.LUM

---
+++ arinc-explorer/src/files/file.rs
@@ -0,0 +1,68 @@
+use std::fmt::{self, Display, Formatter};
+
+use binrw::binrw;
+
+use crate::utils::vec16_to_string;
+
+#[binrw]
+#[brw(big)]
+#[derive(Debug, Clone, Eq, PartialEq, Default)]
+pub struct File {
+    file_pointer: u16,
+    file_name_length: u16, // number of chars
+
+    #[br(count = file_name_length.div_ceil(2))]
+    file_name: Vec<u16>,
+
+    file_pathname_length: u16, // number of chars
+
+    #[br(count = file_pathname_length.div_ceil(2))]
+    file_pathname: Vec<u16>,
+
+    member_sequence_number: u16,
+    file_crc: u16,
+    // Seems like the following fields don't exist
+    // file_check_value_length: u16,
+    // #[br(if(file_check_value_length!=0))]
+    // file_check_value_type: Option<u16>,
+
+    // #[br(if(file_check_value_length!=0), count = file_check_value_length.div_ceil(2))]
+    // file_check_value: Option<Vec<u16>>,
+    // Expansion point no2
+}
+impl File {
+    pub fn get_file_name(&self) -> String {
+        // If filename_length is odd, an extra NUL byte is added at the end.
+        // This function removes the NUL byte if it exists.
+        vec16_to_string(&self.file_name, self.file_name_length as usize)
+    }
+
+    pub fn get_file_pathname(&self) -> String {
+        // If file_pathname_length is odd, an extra NUL byte is added at the end.
+        // This function removes the NUL byte if it exists.
+        vec16_to_string(&self.file_pathname, self.file_pathname_length as usize)
+    }
+}
+impl Display for File {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(
+            f,
+            " \
+            \t{} Offsets\n \
+            \t{} Chars in Filename\n \
+            \t{} Filename\n \
+            \t{} Chars in File Pathname\n \
+            \t{} File Pathname\n \
+            \t{} Sequence number\n \
+            \t0x{:x} CRC\n \
+            \n",
+            self.file_pointer,
+            self.file_name_length,
+            self.get_file_name(),
+            self.file_pathname_length,
+            self.get_file_pathname(),
+            self.member_sequence_number,
+            self.file_crc
+        )
+    }
+}
--- arinc-explorer/src/files/mod.rs
+++ arinc-explorer/src/files/mod.rs
@@ -1,13 +1,16 @@
 use std::fmt::{self, Display, Formatter};
-use std::fs::File;
+use std::fs::File as FsFile; // Rename to avoid conflict with file::File
 use std::io::{Cursor, Read};
 use std::path::Path;
 
 use binrw::{binrw, BinRead};
+use file::File;
 
 use crate::error::FilesLumError;
 use crate::file_class::FileClass;
-use crate::utils::combine_words;
+use crate::utils::{combine_words, vec16_to_string};
+
+mod file;
 
 #[binrw]
 #[brw(big)]
@@ -23,9 +26,31 @@ pub struct FilesLum {
     pointer_to_number_of_media_set_files_lsb: u16,
     pointer_to_user_defined_data_msb: u16,
     pointer_to_user_defined_data_lsb: u16,
-    pointer_to_file_check_value_length_msb: u16,
-    pointer_to_file_check_value_length_lsb: u16,
+    // Seems like pointer_to_file_check_value_length doesn't exist
+    // pointer_to_file_check_value_length_msb: u16,
+    // pointer_to_file_check_value_length_lsb: u16,
     // Expansion point no1
+    media_set_pn_length: u16, // number of chars
+
+    #[br(count = media_set_pn_length.div_ceil(2))]
+    media_set_pn: Vec<u16>, // It is always an even length
+    media_sequence_number_x: u8,
+    number_of_media_set_members_y: u8,
+    number_of_media_set_files: u16,
+
+    #[br(count = number_of_media_set_files)]
+    media_set_files: Vec<File>,
+    // Expansion point no3
+    #[br(if(combine_words(pointer_to_user_defined_data_msb, pointer_to_user_defined_data_lsb)!=0), count = (combine_words(file_length_msb, file_length_lsb) - combine_words(pointer_to_user_defined_data_msb, pointer_to_user_defined_data_lsb) - 1).div_ceil(2))]
+    user_defined_data: Option<Vec<u16>>,
+    // Seems like the following fields don't exist
+    // #[br(if(combine_words(pointer_to_file_check_value_length_msb, pointer_to_file_check_value_length_lsb)!=0))]
+    // file_check_value_length: Option<u16>,
+    // #[br(if(file_check_value_length!=0))]
+    // file_check_value_type: Option<u16>,
+    // #[br(if(file_check_value_length!=0), count = file_check_value_length.div_ceil(2))]
+    // file_check_value: Option<Vec<u16>>,
+    file_crc: u16,
 }
 impl FilesLum {
     /// Constructs a new [`FilesLum`] object.
@@ -42,7 +67,7 @@ impl FilesLum {
     /// Returns [`Err`] if the file is missing or if there is a problem reading
     /// the file into the [`FilesLum`] struct.
     pub fn new(path: &Path) -> Result<Self, FilesLumError> {
-        let mut file = File::open(path)?;
+        let mut file = FsFile::open(path)?;
         let mut buf = Vec::new();
         file.read_to_end(&mut buf)?;
         let files_lum = FilesLum::read(&mut Cursor::new(buf))?;
@@ -98,26 +123,44 @@ impl FilesLum {
         )
     }
 
-    // Returns the offset in 16-bit words from the beginning of the file
     #[must_use]
-    pub fn get_pointer_to_file_check_value_length(&self) -> u32 {
-        combine_words(
-            self.pointer_to_file_check_value_length_msb,
-            self.pointer_to_file_check_value_length_lsb,
-        )
+    pub fn get_media_set_pn(&self) -> String {
+        // If media_set_pn_length is odd, an extra NUL byte is added at the end.
+        // This function removes the NUL byte if it exists.
+        vec16_to_string(&self.media_set_pn, self.media_set_pn_length as usize)
     }
 }
 impl Display for FilesLum {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         write!(
             f,
-            "{} Bytes\n{}\n{} | {} | {} | {:x} Offsets",
+            " \
+            {} Bytes\n \
+            {}\n \
+            {} | {} | {}  Offsets\n \
+            {} Chars in Media Set PN\n \
+            {} Media Set PN\n \
+            {} Media number\n \
+            {} Total Media sets\n \
+            {} Total Media set files\n \
+            {}\n \
+            0x{:x} CRC\n \
+            ",
             self.get_file_length() * 2,
             self.get_file_type_string(),
             self.get_pointer_to_media_set_pn_length(),
             self.get_pointer_to_number_of_media_set_files(),
             self.get_pointer_to_user_defined_data(),
-            self.get_pointer_to_file_check_value_length()
+            self.media_set_pn_length,
+            self.get_media_set_pn(),
+            self.media_sequence_number_x,
+            self.number_of_media_set_members_y,
+            self.number_of_media_set_files,
+            self.media_set_files
+                .iter()
+                .map(|f| format!("{f}"))
+                .fold(String::new(), |acc, arg| acc + arg.as_str()),
+            self.file_crc
         )
     }
 }
@@ -128,13 +171,27 @@ mod tests {
 
     use super::*;
 
-    #[ignore = "File not found"]
     #[test]
     fn test_simple_files_lum() {
         // 1 hex = 4 bits
         // 4 hex = 16 bits = 2 bytes
-        let file = PathBuf::from("../../FILES.LUM");
+        let file = PathBuf::from("../test-data/FILES.LUM");
         let files_lum = FilesLum::new(file.as_path()).unwrap();
         println!("{files_lum}");
+
+        assert_eq!(files_lum.get_file_length(), 300);
+        assert_eq!(files_lum.media_file_format_verion, 0x8002);
+        assert_eq!(files_lum.get_pointer_to_media_set_pn_length(), 0x9);
+        assert_eq!(files_lum.get_pointer_to_number_of_media_set_files(), 0x10);
+        assert_eq!(files_lum.get_pointer_to_user_defined_data(), 0x0);
+        assert_eq!(files_lum.media_set_pn_length, 10);
+        assert_eq!(files_lum.media_set_pn.len(), 5);
+        assert_eq!(files_lum.get_media_set_pn(), "ABC1813001");
+        assert_eq!(files_lum.media_sequence_number_x, 1);
+        assert_eq!(files_lum.number_of_media_set_members_y, 1);
+        assert_eq!(files_lum.number_of_media_set_files, 14);
+        assert_eq!(files_lum.media_set_files.len(), 14);
+        assert_eq!(files_lum.user_defined_data, None);
+        assert_eq!(files_lum.file_crc, 0x3BE8);
     }
 }
