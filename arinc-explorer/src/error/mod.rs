a70020d7a426aa3d52d6fdcd8725ceecb5fc2a49
eagle941 <8973725+Eagle941@users.noreply.github.com>
2025-08-22 23:00:49 +0100

    chore: removed Error enum because unused

--- arinc-explorer/src/error/mod.rs
+++ arinc-explorer/src/error/mod.rs
@@ -1,16 +1,5 @@
 mod files_lum;
 mod loads_lum;
 
-use thiserror::Error;
-
 pub use self::files_lum::Error as FilesLumError;
 pub use self::loads_lum::Error as LoadsLumError;
-
-#[derive(Debug, Error)]
-pub enum Error {
-    #[error(transparent)]
-    LoadsLum(#[from] LoadsLumError),
-
-    #[error(transparent)]
-    FilesLum(#[from] FilesLumError),
-}
